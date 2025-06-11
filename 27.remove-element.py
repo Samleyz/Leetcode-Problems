038f4cb1a98c37188bc009eac04088ef41c1d95f
Samleyz <samuel.cehlarik@gmail.com>
2025-06-11 16:31:44 +0200

    Update 27.remove-element.py

---
+++ 27.remove-element.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+            for i in range(len(nums)):
+                if val in nums:
+                    nums.remove(val)
+            return len(nums)
